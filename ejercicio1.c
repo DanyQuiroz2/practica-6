94c06d1af21f636155eb25314782b6d8d1503858
DanyQuiroz2 <dqc220618@gmail.com>
2025-03-20 14:49:17 -0600

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,1 +1,25 @@
+//Danila QYuroz Cruz
 
+Stdio.h
+int
+int main ()
+{
+    if()
+    {
+    }
+    else
+    {
+        if ()
+        {
+            
+        }
+    }
+    return 0;
+}
+while ()
+{
+    
+    
+}
+printf ("%i",res);
+scanf ("%i",&num);
