6583492c5ad41e2a9348a83357be5c6accf44bbc
DanyQuiroz2 <dqc220618@gmail.com>
2025-05-02 23:46:48 -0600

    ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,25 +1,24 @@
 //Daniela Quiroz Cruz
 
 #include <stdio.h>
-int
-int main ()
-{
-    if()
-    {
-    }
-    else
-    {
-        if ()
-        {
-            
-        }
-    }
+
+int main() {
+    int numero;
+    float decimal = 3.14;
+    char letra = 'A';
+    char nombre[50];
+
+    printf("Introduce un número entero: ");
+    scanf("%d", &numero);
+
+    printf("Introduce tu nombre: ");
+    scanf("%s", nombre);
+
+    printf("\n--- Resultados ---\n");
+    printf("Número entero: %d\n", numero);
+    printf("Número decimal: %.2f\n", decimal);
+    printf("Letra: %c\n", letra);
+    printf("Nombre: %s\n", nombre);
+
     return 0;
 }
-while ()
-{
-    
-    
-}
-printf ("%i",res);
-scanf ("%i",&num);
